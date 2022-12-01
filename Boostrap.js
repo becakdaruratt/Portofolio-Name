6dbe1859d584f4e4046f59720f84a1c451889e9a
becakdaruratt <pwdarurat@gmail.com>
2022-12-01 08:44:38 +0700

    Menambahkan file portofolio

---
+++ Boostrap.js
@@ -0,0 +1,24 @@
+const container = document.querySelectorAll('.gallery');
+const besar = document.querySelector('.jumbo');
+const thumbs = document.querySelectorAll('.gallery-img');
+container.forEach(function(e){
+    
+    e.addEventListener('click',function(iven){
+        if(iven.target.className == 'anak'){
+            besar.src = iven.target.src;
+            besar.classList.add('fade');
+            setTimeout(function(){
+                besar.classList.remove('fade');
+         
+            }, 500);
+            
+        }   
+        thumbs.forEach(function(asli){
+            asli.className = 'anak';
+        });
+        
+        iven.target.classList.add('active');
+        iven.preventDefault();
+        iven.stopPropagation();
+    });
+});
